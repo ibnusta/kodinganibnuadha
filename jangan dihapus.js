d0973fc0a4e02bd3358948ab4ad7eef34e238570
Ibnu adha <69663123+ibnuadhaart@users.noreply.github.com>
2020-10-18 11:00:11 +0700

    Add files via upload

---
+++ jangan dihapus.js
@@ -0,0 +1,7 @@
+<!-- Ciee Ngintip -ini khusus punya bang ibnuadha-->
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://ibnuadha.blogspot.com/") {
+        window.location.href = "https://ibnuadha.blogspot.com"; }});
+//]]>
+</script>
